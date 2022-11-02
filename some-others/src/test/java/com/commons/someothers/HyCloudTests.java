edf35601f050eca89dfe362a6e2a6c77ea1f7477
迟书朋 <chi.shupeng@trs.com.cn>
2022-11-02 09:36:12 +0800

    hycloudtest

---
+++ some-others/src/test/java/com/commons/someothers/HyCloudTests.java
@@ -0,0 +1,21 @@
+package com.commons.someothers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyCloudTests {
+
+
+
+    @Test
+    void chineseToPinYinFirstChar() {
+
+
+    }
+
+
+
+
+
+}
